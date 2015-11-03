c4d3999916e0c40798e16aef6c1debafbac9f20b
Wisney <wisneytaas@gmail.com>
2015-11-03 11:20:15 -0200

    Validador de CPF

--- app.js
+++ app.js
@@ -14,5 +14,28 @@ var app = angular.module('formExample', []);
     this.reset();
   }]);
 
-  
+  app.directive('validaCpf', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      ctrl.$validators.validaCpf = function(modelValue, viewValue) {
+        strCPF = viewValue;
+        var Soma;
+        var Resto;
+        Soma = 0;
+        if (strCPF == "00000000000") return false;
+        for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+          Resto = (Soma * 10) % 11;
+        if ((Resto == 10) || (Resto == 11)) Resto = 0;
+        if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+        Soma = 0;
+        for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+          Resto = (Soma * 10) % 11;
+        if ((Resto == 10) || (Resto == 11)) Resto = 0;
+        if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+        return true;
+      };
+    }
+  };
+});
 
